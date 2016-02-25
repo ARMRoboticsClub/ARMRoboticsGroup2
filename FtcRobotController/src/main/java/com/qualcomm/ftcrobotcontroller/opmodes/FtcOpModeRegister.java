142e4743bca47ac4fb80119f1b1cc883a7eba7fa
dargre01 <darin.greene@arm.com>
2016-02-25 12:05:39 -0600

    New op modes

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -57,8 +57,11 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     manager.register("NullOp", NullOp.class);
 
+
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
     manager.register("K9TeleOp", K9TeleOp.class);
+    manager.register("Group2TeleOp", Group2TeleOp.class);
+    manager.register("Group2Autonomous", Group2Auto.class);
     manager.register("K9Line", K9Line.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
